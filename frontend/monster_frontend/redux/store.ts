fbcb7a2919c49686d15a2d4c89e2fc3c60704a04
Cameron Smart <smartcameron@gmail.com>
2023-06-07 14:06:37 -0700

    setup nextjs

---
+++ frontend/monster_frontend/redux/store.ts
@@ -0,0 +1,10 @@
+// redux/store.js
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {
+    // your reducers will go here
+  },
+})
+
+export default store;
