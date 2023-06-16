2bf899895685f929eaf3c03dcb8e81386e826649
Cameron Smart <smartcameron@gmail.com>
2023-06-16 01:37:17 -0700

    nav bar

--- frontend/monster_frontend/app/layout.tsx
+++ frontend/monster_frontend/app/layout.tsx
@@ -10,14 +10,14 @@ const NavBar = (): ReactElement => {
     <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
       <Link href="/">
         <div className="flex items-center flex-shrink-0 text-white mr-6 cursor-pointer">
-          <span className="font-semibold text-xl tracking-tight">Monster Adventure</span>
+          <span className="font-semibold text-xl tracking-tight">Home</span>
         </div>
       </Link>
-      <Link href="/adventures">
+      {/* <Link href="/adventures">
         <div className="flex items-center flex-shrink-0 text-white mr-6 cursor-pointer">
           <span className="font-semibold text-xl tracking-tight">Adventures</span>
         </div>
-      </Link>
+      </Link> */}
       <Link href="/monsters">
         <div className="flex items-center flex-shrink-0 text-white mr-6 cursor-pointer">
           <span className="font-semibold text-xl tracking-tight">Monsters</span>
