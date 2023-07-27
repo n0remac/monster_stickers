cea7d684a1d1430d38ba1a882af2245cfc27cecb
Cameron Smart <smartcameron@gmail.com>
2023-07-26 21:32:56 -0700

    try to fix cors

--- monster_project/monster_project/settings.py
+++ monster_project/monster_project/settings.py
@@ -144,4 +144,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
