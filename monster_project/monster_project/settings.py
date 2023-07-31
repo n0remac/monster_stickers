c6be55ef2be5d5af2a499057172d5d723e250e00
Cameron Smart <smartcameron@gmail.com>
2023-07-30 17:28:48 -0700

    save

--- monster_project/monster_project/settings.py
+++ monster_project/monster_project/settings.py
@@ -33,7 +33,7 @@ DEBUG = env('DEBUG') != '0'
 
 HOST_IP = env('HOST_IP')
 
-ALLOWED_HOSTS = [HOST_IP, '127.0.0.1', '165.232.147.189', 'fabledfusion.com', 'www.fabledfusion.com']
+ALLOWED_HOSTS = [HOST_IP, '127.0.0.1', '165.232.147.189', 'fabledfusion.com', 'www.fabledfusion.com', 'host.docker.internal']
 
 # Application definition
 
